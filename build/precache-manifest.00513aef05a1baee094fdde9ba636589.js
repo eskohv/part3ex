4e27030ddc677424f9cf71583eb9655a8abe1e69
Eetu Kohvakka <eetu.s.kohvakka@utu.fi>
2020-11-20 14:41:44 +0200

    build

---
+++ build/precache-manifest.00513aef05a1baee094fdde9ba636589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04152eac2a57affd3ba9250359a392a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6447b5690e0a5f31b04a",
+    "url": "/static/css/main.d24de726.chunk.css"
+  },
+  {
+    "revision": "f27baae0c693f5ddcc1c",
+    "url": "/static/js/2.4f3b5c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4f3b5c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6447b5690e0a5f31b04a",
+    "url": "/static/js/main.2117b1f8.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
